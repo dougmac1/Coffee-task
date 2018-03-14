ef086d45331495ead0bb650b140338cb9b8eadbb
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-03-14 14:57:52 +0000

    Added Brew method and Grind method

--- src/main/scala/CoffeeMaker.scala
+++ src/main/scala/CoffeeMaker.scala
@@ -1,24 +1,51 @@
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+import scala.util.control.NonFatal
 case class Water(temperature: Int)
 abstract class Milk
 object WholeMilk extends Milk
 object SemiSkimmedMilk extends Milk
 case class FrothedMilk(milk: Milk) extends Milk
-
-
-
+case class CoffeeBeans (beans: String)
+case class GroundCoffee(coffeeBeans: CoffeeBeans)
+case	class	BrewingException(msg:	String)	extends	Exception("The water is too cold")
+case class Coffee(water: Water, groundCoffee: GroundCoffee, frothedMilk: Option[FrothedMilk])
 
 
 
 object CoffeeMaker extends App {
 
+  def heatWater(water: Water, temp : Int = 40) = {
+    println(s"Water has heated to $temp")
+    Water(temp)
+  }
 
-  def heatWater(water: Water) = Water(50)
-
-
-
-  def froth(milk: Milk) = milk match {
+  def froth(milk: Milk) : FrothedMilk = milk match {
     case WholeMilk => FrothedMilk(WholeMilk)
     case SemiSkimmedMilk => throw new IllegalArgumentException("Can't froth Semi-Skimmed Milk")
+
   }
+
+  def grind(beans: CoffeeBeans) = {
+    "Successfully Ground Beans"
+    GroundCoffee(beans)
+  }
+
+  def brew(water: Water, groundCoffee: GroundCoffee, option: Option[FrothedMilk]) : Coffee = {
+    Coffee(water, groundCoffee, option)
+  }
+
+  val frothed = Try(froth(SemiSkimmedMilk))
+  val water = Water(20)
+  val heated = heatWater(water, 80)
+  val beans = CoffeeBeans("Arrabica beans")
+  val ground = grind(beans)
+
+  frothed match {
+    case Success(m) =>
+      brew(heated, ground, Some(m))
+    case Failure(e) =>
+      brew(heated, ground, None)
+  }
+
 }
--- src/test/scala/CoffeeMakerSpec.scala
+++ src/test/scala/CoffeeMakerSpec.scala
@@ -14,9 +14,21 @@ class CoffeeMakerSpec extends WordSpec with MustMatchers {
       intercept[IllegalArgumentException](CoffeeMaker.froth(SemiSkimmedMilk))
     }
 
-    "Return a new instance of water when heating water" in {
+    "Return a new instance of water with a new temperature when heating water" in {
 
-      CoffeeMaker.heatWater(Water(20)) mustEqual Water(50)
+      CoffeeMaker.heatWater(Water(20), 80) mustEqual Water(80)
+    }
+
+    "Return specified water temp when temp is not specified" in {
+      CoffeeMaker.heatWater(Water(20)) mustEqual Water(40)
+    }
+
+    "Return GroundCoffee when given Arabica Beans" in {
+      CoffeeMaker.grind("Arabica Beans") mustEqual GroundCoffee
+    }
+
+    "Return BrewingException when temperature specified is less than 40" in {
+      CoffeeMaker.brew() mustEqual
     }
   }
 }
