32fea6186455870c799aa9ccba84301e1242a2aa
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-03-14 11:23:30 +0000

    Started Frothed Milk Test

--- src/test/scala/CoffeeMakerSpec.scala
+++ src/test/scala/CoffeeMakerSpec.scala
@@ -2,4 +2,8 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class CoffeeMakerSpec extends WordSpec with MustMatchers {
 
+  CoffeeMaker must {
+
+    "return frothedMilk when frothing WholeMilk"
+  }
 }
